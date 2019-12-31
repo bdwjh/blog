bb88dc749a0d0c0b3b0b7cdd189f61a9d7c752a8
bdwjh <bdwjh@163.com>
2019-12-31 09:47:55 +0800

    实现局部视图

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,11 @@
+<footer class="footer">
+    <img class="brand-icon" courses="" https:="" laravel-china.org="" src="https://iocaffcdn.phphub.org/uploads/sites/KDiyAbV0hj1
+        <a href=" target="_blank">
+        刻意练习，每日精进
+    </img>
+</footer>
+<div class="float-right">
+    <a href="/about">
+        关于
+    </a>
+</div>
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,9 +1,14 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<title>@yield('title', 'Weibo App') - 我的练习程序</title>
-	</head>
-	<body>
-		@yield('content')
-	</body>
+    <head>
+        <title>
+            @yield('title', 'Weibo App') - 我的练习程序
+        </title>
+        <link rel="stylesheet" href="css/app.css">
+    </head>
+    <body>
+        @include('layouts._header')
+        @yield('content')
+        @include('layouts._footer')
+    </body>
 </html>
